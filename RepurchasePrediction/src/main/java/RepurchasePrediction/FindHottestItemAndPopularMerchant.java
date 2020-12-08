fc58f1467f91c5b601f537d5516c57b1aa3b1438
jinkq <1154609706@qq.com>
2020-12-08 20:48:47 +0800

    item-count

--- RepurchasePrediction/src/main/java/RepurchasePrediction/FindHottestItemAndPopularMerchant.java
+++ RepurchasePrediction/src/main/java/RepurchasePrediction/FindHottestItemAndPopularMerchant.java
@@ -1,13 +1,95 @@
 package RepurchasePrediction;
 
-/**
- * Hello world!
- *
- */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.StringUtils;
+
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.map.InverseMapper;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.fs.FileSystem;
+
 public class FindHottestItemAndPopularMerchant
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static class FindHottestItemMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+
+        @Override
+        public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+            String[] line = value.toString().split(",");
+            if(line[1].length() != 0 && !(line[1].equals("item_id")) && line[6].length() != 0 && !(line[6].equals("0") ))
+                context.write(new Text(line[1]), one);
+        }
+    }
+
+    public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                        Context context
+                        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main( String[] args ) throws Exception {
+        Configuration conf = new Configuration();
+        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
+        String[] remainingArgs = optionParser.getRemainingArgs();
+        if (remainingArgs.length != 2) {
+            System.err.println("Usage: FindCommonFriends <in> <out>");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "FindHottestItemAndPopularMerchant");
+        job.setJarByClass(FindHottestItemAndPopularMerchant.class);
+        job.setMapperClassFindHottestItemMapper.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        List<String> otherArgs = new ArrayList<String>();
+        for (int i=0; i < remainingArgs.length; ++i) {
+            otherArgs.add(remainingArgs[i]);
+        }
+        FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
+
+        Path outputPath = new Path(otherArgs.get(1));
+        FileSystem fileSystem = outputPath.getFileSystem(conf);
+        if (fileSystem.exists(outputPath)) {
+            fileSystem.delete(outputPath, true);
+        }
+        FileOutputFormat.setOutputPath(job, outputPath);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
