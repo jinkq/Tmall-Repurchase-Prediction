1b59968c38514ab15b71e6f9e6b0484c272de74e
jinkq <1154609706@qq.com>
2020-12-08 11:29:14 +0800

    init

---
+++ RepurchasePrediction/src/main/java/RepurchasePrediction/FindHottestItemAndPopularMerchant.java
@@ -0,0 +1,13 @@
+package RepurchasePrediction;
+
+/**
+ * Hello world!
+ *
+ */
+public class FindHottestItemAndPopularMerchant
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ RepurchasePrediction/src/test/java/RepurchasePrediction/FindHottestItemAndPopularMerchantTest.java
@@ -0,0 +1,20 @@
+package RepurchasePrediction;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class FindHottestItemAndPopularMerchantTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
