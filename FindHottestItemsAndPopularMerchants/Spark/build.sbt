7e65496c02d67c08c78116e3ff7b74c6e0161311
$GIT_USERNAME <$GIT_USEREMAIL>
2020-12-16 07:55:57 +0000

    scala word count

---
+++ FindHottestItemsAndPopularMerchants/Spark/build.sbt
@@ -0,0 +1,4 @@
+name := "Find Hottest Items And Popular Merchants"
+version := "1.0"
+scalaVersion := "2.11.12"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
---
+++ FindHottestItemsAndPopularMerchants/Spark/src/main/scala/ScalaWordCount.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+object ScalaWordCount {
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Usage: <input path> <output path>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf().setAppName("Scala_WordCount_Demo")
+    val sc = new SparkContext(conf)
+    val line = sc.textFile(args(0)).split(" ")(1)
+    
+    // val word = line.map(_.split(" "))
+    // val counts = line.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_)
+    val counts = line.map((_, 1)).reduceByKey(_+_)
+    counts.collect()
+  
+    counts.saveAsTextFile(args(1))
+
+    sc.stop()
+  }
+}
