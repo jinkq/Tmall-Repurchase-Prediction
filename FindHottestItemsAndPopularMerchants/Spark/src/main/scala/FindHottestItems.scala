168346be1fdb5f6d6e5a217a5d8fedc74e986547
$GIT_USERNAME <$GIT_USEREMAIL>
2020-12-17 14:55:22 +0000

    find hottest items but not format

---
+++ FindHottestItemsAndPopularMerchants/Spark/src/main/scala/FindHottestItems.scala
@@ -0,0 +1,39 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+
+object FindHottestItems {
+  def action123(line: String): String = {
+    val words = line.split(",")
+    if(words(6) == "1" || words(6) == "2" || words(6) == "3"){ //添加购物⻋、购买、添加收藏夹
+      return words(1)
+    }
+    else{
+      return ""
+    }
+  }
+
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Usage: <input path> <output path>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf().setAppName("FindHottestItems")
+    val sc = new SparkContext(conf)
+    val line = sc.textFile(args(0))
+    // val line = sc.textFile("../../../input_test/test.txt")
+    
+    // val itemPairs = line.map(_.split(",")(1)).map((_, 1)).reduceByKey(_+_)
+    val itemPairs = line.map(line => action123(line)).map((_, 1)).filter{case (key, value) => key != ""}.reduceByKey(_+_)
+    val sortedItemPairs = itemPairs.map(line =>(line._2,line._1)).sortByKey(false).map(line =>(line._2,line._1)).take(100)
+    
+    val sortedItems = sc.parallelize(sortedItemPairs)//将数组转化为RDD
+  
+    sortedItems.saveAsTextFile(args(1))
+    // itemPairs.saveAsTextFile("../../../output")
+
+    sc.stop()
+  }
+}
