85f5da6e50729843384d004b516766ff6b828345
$GIT_USERNAME <$GIT_USEREMAIL>
2020-12-17 08:41:05 +0000

    sort item

--- FindHottestItemsAndPopularMerchants/Spark/src/main/scala/ScalaWordCount.scala
+++ FindHottestItemsAndPopularMerchants/Spark/src/main/scala/ScalaWordCount.scala
@@ -2,23 +2,24 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
-object ScalaWordCount {
+object FindHottestItems {
   def main(args: Array[String]) {
     if (args.length < 2) {
       System.err.println("Usage: <input path> <output path>")
       System.exit(1)
     }
 
-    val conf = new SparkConf().setAppName("Scala_WordCount_Demo")
+    val conf = new SparkConf().setAppName("FindHottestItems")
     val sc = new SparkContext(conf)
     val line = sc.textFile(args(0))
+    // val line = sc.textFile("../../../input_test/test.txt")
     
-    // val word = line.map(_.split(" "))
-    val counts = line.flatMap(_.split(",")(0)).map((_, 1)).reduceByKey(_+_)
-    // val counts = line.map((_, 1)).reduceByKey(_+_)
-    counts.collect()
+    val itemPairs = line.map(_.split(",")(1)).map((_, 1)).reduceByKey(_+_)
+    val sortedItemPairs = itemPairs.map(line =>(line._2,line._1)).sortByKey(false).map(line =>(line._2,line._1))
+    sortedItemPairs.collect()
   
-    counts.saveAsTextFile(args(1))
+    sortedItemPairs.saveAsTextFile(args(1))
+    // itemPairs.saveAsTextFile("../../../output")
 
     sc.stop()
   }
