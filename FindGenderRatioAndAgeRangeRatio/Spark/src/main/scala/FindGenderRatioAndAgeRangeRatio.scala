d27e03b92ec190c2f698ff3e981b3bc728471514
$GIT_USERNAME <$GIT_USEREMAIL>
2020-12-22 03:57:51 +0000

    age range test success

--- FindGenderRatioAndAgeRangeRatio/Spark/src/main/scala/FindGenderRatioAndAgeRangeRatio.scala
+++ FindGenderRatioAndAgeRangeRatio/Spark/src/main/scala/FindGenderRatioAndAgeRangeRatio.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+import org.apache.spark.rdd._
 
 
 object FindGenderRatioAndAgeRangeRatio {
@@ -29,7 +30,7 @@ object FindGenderRatioAndAgeRangeRatio {
   
   def getAgeUsers(line: String, ageRange: String): String = { //获得对应ageRange的users
     val words = line.split(",")
-    if(ageRange == words(2)){
+    if(ageRange == words(1)){
       return words(0)
     }
     else{
@@ -51,8 +52,8 @@ object FindGenderRatioAndAgeRangeRatio {
     val log = line.filter(x => x.split(",").length==7)//来自log表
     
     //统计购买了商品的男女比例
-    val maleUsers = info.map(info => getGenderUsers(info, "male")).map((_, 1)).filter{case (key, value) => key != ""}.reduceByKey(_+_)
-    val femaleUsers = info.map(info => getGenderUsers(info, "female")).map((_, 1)).filter{case (key, value) => key != ""}.reduceByKey(_+_)
+    val maleUsers = info.map(info => getGenderUsers(info, "male")).map((_, 1)).filter{case (key, value) => key != ""}
+    val femaleUsers = info.map(info => getGenderUsers(info, "female")).map((_, 1)).filter{case (key, value) => key != ""}
     
     val buyUsers = log.map(log => getBuyUsers(log)).filter(user => user != "").distinct().map((_, 1))
     val maleBuyUsers = maleUsers.join(buyUsers).keys
@@ -66,14 +67,31 @@ object FindGenderRatioAndAgeRangeRatio {
     genderCount.saveAsTextFile(args(1) + "/gender")
 
     //统计购买了商品的买家年龄段的⽐例
-    val ageRangeArray = info.filter(info => (info.split(",")(1) != "0" && info.split(",")(1) != "NULL")).map(info => info.split(",")(1)).distinct()
+    // val validAgeRange = Array("1", "2", "3", "4", "5", "6", "7", "8")
+    val validAgeRangeArray = Array(("<18", "1"), ("[18, 24]", "2"), ("[25, 29]", "3"), ("[30, 34]", "4"), ("[35, 39]", "5"), ("[40, 49]", "6"), ("≥50", "7"), ("≥50", "8"))
     
-    var list:List
-    val list1 = List("abc","xyz")
-    val list2 = List("efg","his")
-    list = list1::list2::list
-    list_=sc.parallelize(list)
-    list_.saveAsTextFile(args(1)+"test")
+    // val ageRangeArray = info.filter(info => (validAgeRange contains info.split(",")(1))).map(info => info.split(",")(1)).distinct().collect()
+
+    var list:List[String]=List()
+
+    for(ageRange <- validAgeRangeArray){
+      val ageUsers = info.map(info => getAgeUsers(info, ageRange._2)).map((_, 1)).filter{case (key, value) => key != ""}
+      val ageBuyUsers = ageUsers.join(buyUsers).keys
+      // val ageBuyUserCountPair = "Age Range = " + String.valueOf(ageRange)+": " + String.valueOf(ageBuyUsers.count())
+      val ageBuyUserCountPair = "年龄区间" + String.valueOf(ageRange._1) + ": " + String.valueOf(ageBuyUsers.count())
+      // val ageBuyUserCountPairs = ageBuyUsers.map(ageBuyUser => (ageRange, ageBuyUsers.count()))
+      // val formattedAgeBuyUserCountPairs = ageBuyUserCountPairs.map(ageBuyUserCountPair => "Age Range = " + String.valueOf(ageBuyUserCountPair._1)+": "+String.valueOf(ageBuyUserCountPair._2))
+      // val ageBuyUsersCount = "Age Range = " + String.valueOf(ageRange)+": " + String.valueOf(ageUsers.join(buyUsers).keys.count())
+      list = ageBuyUserCountPair :: list
+    }
+
+    // for(i <- list){
+    //   println(i)
+    //   println("\n")
+    // }
+    
+    val list3=sc.parallelize(list.reverse)
+    list3.saveAsTextFile(args(1)+"/age range")
     // val youngUsers = info.map(info => getAgeUsers(info)).filter(user => user != "")
     // val actionUserSellerPairs = log.map(log => getActionUserSellerPairs(log)).filter{case (key, value) => key != ""}
     // val youngActionUsers = youngUsers.intersection(actionUserSellerPairs.keys).distinct().map((_,1))//符合actionType的年轻人
