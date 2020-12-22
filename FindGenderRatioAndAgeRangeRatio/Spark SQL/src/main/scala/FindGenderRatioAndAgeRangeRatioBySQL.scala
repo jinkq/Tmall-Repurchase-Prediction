af2b25e37b3d47bec625a396f963da03b21d385d
$GIT_USERNAME <$GIT_USEREMAIL>
2020-12-22 12:40:44 +0000

    tmp

--- FindGenderRatioAndAgeRangeRatio/Spark SQL/src/main/scala/FindGenderRatioAndAgeRangeRatioBySQL.scala
+++ FindGenderRatioAndAgeRangeRatio/Spark SQL/src/main/scala/FindGenderRatioAndAgeRangeRatioBySQL.scala
@@ -11,12 +11,15 @@ object FindGenderRatioAndAgeRangeRatioBySQL {
       System.exit(1)
     }
 
+    val infoPath = "/user_info_format1.csv"
+    val logPath = "/test.csv"
+
     val conf = new SparkConf().setAppName("FindGenderRatioAndAgeRangeRatioBySQL")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
     
-    val infoDf = sqlContext.load("com.databricks.spark.csv", Map("path" -> (args(0) + "/user_info_format1.csv"), "header"-> "true"))
-    val logDf = sqlContext.load("com.databricks.spark.csv", Map("path" -> (args(0) + "/test.csv"), "header"-> "true"))
+    val infoDf = sqlContext.load("com.databricks.spark.csv", Map("path" -> (args(0) + infoPath), "header"-> "true"))
+    val logDf = sqlContext.load("com.databricks.spark.csv", Map("path" -> (args(0) + logPath), "header"-> "true"))
     
     val joinExpression = infoDf.col("user_id") === logDf.col("user_id")
     val infoLogDf = infoDf.join(logDf, joinExpression)
@@ -34,7 +37,17 @@ object FindGenderRatioAndAgeRangeRatioBySQL {
     genderRatio.saveAsTextFile(args(1) + "/gender")
 
     //统计购买了商品的买家年龄段的⽐例
+    val validAgeRangeArray = Array(("<18", "1"), ("[18, 24]", "2"), ("[25, 29]", "3"), ("[30, 34]", "4"), ("[35, 39]", "5"), ("[40, 49]", "6"), ("≥50", "7"), ("≥50", "8"))
 
+    var ageCountPairList:List[(String, Float)]=List()
+    var ageRatioList:List[String]=List()
+
+    for(ageRange <- validAgeRangeArray){
+      val ageDf = infoLogDf.where("age_range = " + ageRange._2 + " and action_type = \"2\"").dropDuplicates("user_id")
+      val ageCount = ageDf.count().toFloat
+      val ageCountPair = (ageRange._1, ageCount)
+      ageCountPairList = ageCountPair :: ageCountPairList
+    }
 
     sc.stop()
   }
