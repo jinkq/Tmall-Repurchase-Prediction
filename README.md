f491329518cec6df6406c43088c3cb06f728fa83
jkq <1154609706@qq.com>
2020-12-27 23:02:35 +0800

    task 4 running

--- README.md
+++ README.md
@@ -6,19 +6,28 @@
 3. 基于Hive或者Spark SQL查询双十一购买了商品的男女比例，以及购买了商品的买家年龄段的比例；
 4. 预测给定的商家中，哪些新消费者在未来会成为忠实客户，即需要预测这些新消费者在6个月内再次购买的概率。基于Spark MLlib编写程序预测回头客，评估实验结果的准确率。
 
+## 说明
+
+* MapReduce使用Java语言，用Maven管理
+* Spark使用Scala语言，用sbt管理
+
 ## 1. 统计双十一最热门的商品和最受年轻人(age<30)关注的商家
 ### MapReduce
-#### 统计最热门商品
-1. 新建job`measureItemsPopularityJob`，读取数据文件`user_log_format1.csv`，对于数据文件中`action_type`为1、2或3的样本的`item_id`字段进行计数，输出<key, value>=<item_id, 出现次数>，保存至临时文件夹`tempDir`
+#### 输入、输出
+* 输入：input文件夹（仅包含`user_log_format1.csv`和`user_info_format1.csv`）
+* 输出：FindHottestItemsAndPopularMerchants/MapReduce/output
+#### 设计思路
+##### 统计最热门商品
+1. 新建job`measureItemsPopularityJob`，读取数据文件`user_log_format1.csv`，对于数据文件中`action_type`为1、2或3且`time_stamp`为1111的样本的`item_id`字段进行计数，输出<key, value>=<item_id, 出现次数>，保存至临时文件夹`tempDir`
 2. 新建job`sortItemsPopularityJob`，读取`tempDir`中的数据，利用InverseMapper将键和值调换位置，再根据新的键进行降序排序，保留前100条数据作为输出
-#### 统计最受年轻人(age<30)关注的商家
+##### 统计最受年轻人(age<30)关注的商家
 1. 自定义数据类型UserLog，属性为商家ID`sellerId`（String，默认为""）和买家年龄是否小于30`userAge`（Boolean，默认为false）
 2. 新建job`mergeTableJob`
     1. 在Mapper中读取数据文件`user_log_format1.csv`和`user_info_format1.csv`，根据读取行按照","分割的数据长度判断该行来自于哪个csv。
-        * 对于来自`user_log_format1.csv`的样本，若其`action_type`为1、2或3，则新建`UserLog`对象`userLog`，并设置其`sellerId`属性为样本的`seller_id`字段值，输出<key, value>=<user_id, userLog>；
+        * 对于来自`user_log_format1.csv`的样本，若其`action_type`为1、2或3且`time_stamp`为1111，则新建`UserLog`对象`userLog`，并设置其`sellerId`属性为样本的`seller_id`字段值，输出<key, value>=<user_id, userLog>；
         * 对于来自`user_info_format1.csv`的样本，若其`age_range`为1、2或3，则新建`UserLog`对象，并设置其`userAge`属性为true，输出<key, value>=<user_id, userLog>
     2. 在Reducer中，检测同一user_id的一系列UserLog对象，
-        * 若对象的`sellerId`属性不为空字符串，则认定该user买了商品，并将该sellerId加入列表`sellers`；
+        * 若对象的`sellerId`属性不为空字符串，则认定该user在双十一当天买了商品，并将该sellerId加入列表`sellers`；
         * 若对象的`userAge`属性为true，则认定该user为年轻人。
 
         遍历完所有UserLog对象，若该user既买了商品，又是年轻人，则将列表`sellers`中的每一个seller的ID作为key输出，即输出<key, value>=<seller_id, NullWritable>，删除原有的tempDir，并将输出写入临时文件夹tempDir。
@@ -26,16 +35,145 @@
 4. 新建job`sortMerchantsPopularityJob`，除了输出的文字，其他均与`sortItemsPopularityJob`相同
 #### 运行方法
 `hadoop jar <FindHottestItemsAndPopularMerchants-1.0-SNAPSHOT.jar路径> <input> <output>`
-* \<input>中含user_log_format1.csv和user_info_format1.csv
+
+* \<input>中仅含user_log_format1.csv和user_info_format1.csv
 * \<output>将含文件夹hottest items和popular merchants among young，分别存储双十一最热门的商品和最受年轻人(age<30)关注的商家
 ### Spark
+#### 输入、输出
+* 输入：input文件夹（仅包含`user_log_format1.csv`和`user_info_format1.csv`）
+* 输出：FindHottestItemsAndPopularMerchants/Spark/output
+#### 设计思路
+1. 读取数据文件`user_log_format1.csv`和`user_info_format1.csv`，根据读取行按照","分割的数据长度判断该行来自于哪个csv
+##### 统计最热门商品
+2. 从log表（user_log_format1.csv）中用map筛选出所有`action_type`为1、2或3且`time_stamp`为1111的行，对这些行的`item_id`字段进行计数并降序排序
+##### 统计最受年轻人(age<30)关注的商家
+3. 从info表（user_info_format1.csv）中用map筛选出所有`age_range`为1、2或3的行所对应的`user_id`，将这些user与统计最热门商品中获得的双十一当天购买了商品的users取交集
+4. 对于交集中user对应的`seller_id`字段进行计数并降序排序
 
-## 2. 统计双⼗⼀购买了商品的男女⽐例，以及购买了商品的买家年龄段的比例
+#### 运行方法
+
+`spark-submit --class "FindHottestItemsAndPopularMerchants" --master local <find-hottest-items-and-popular-merchants_2.11-1.0.jar路径> <input> <output>`
+
+* \<input>中仅含user_log_format1.csv和user_info_format1.csv
+* \<output>将含文件夹hottest items和popular merchants among young，分别存储双十一最热门的商品和最受年轻人(age<30)关注的商家
+
+## 2、3. 统计双十一购买了商品的男女比例，以及购买了商品的买家年龄段的比例
 ### Spark
+#### 输入、输出
+* 输入：input文件夹（仅包含`user_log_format1.csv`和`user_info_format1.csv`）
+* 输出：FindGenderRatioAndAgeRangeRatio/Spark/output
+#### 设计思路
+1. 读取数据文件`user_log_format1.csv`和`user_info_format1.csv`，根据读取行按照","分割的数据长度判断该行来自于哪个csv
+2. 从log表（user_log_format1.csv）中用map筛选出所有`action_type`为1、2或3且`time_stamp`为1111的users
+3. 从info表（user_info_format1.csv）中用map筛选出所有对应性别或年龄的users与上述users取交集，再计算对应的人数及比例
+
+#### 运行方法
+
+`spark-submit --class "FindGenderRatioAndAgeRangeRatio" --master local <find-gender-ratio-and-age-range-ratio_2.11-1.0.jar路径> <input> <output>`
+
+* \<input>中仅含user_log_format1.csv和user_info_format1.csv
+* \<output>将含文件夹age range和gender，分别存储对性别及年龄段的人数、比例的统计
 
 ### Spark SQL
 
-## 3. MLlib预测新消费者在6个月内再次购买的概率
+#### 输入、输出
+
+* 输入：input文件夹（仅包含`user_log_format1.csv`和`user_info_format1.csv`）
+* 输出：FindGenderRatioAndAgeRangeRatio/Spark SQL/output
+
+#### 设计思路
+
+与普通Spark做法的区别是：使用DataFrame作为基础数据结构，用where语句进行条件的筛选，用join语句进行DataFrame的合并
+
+#### 运行方法
+
+`spark-submit --class "FindGenderRatioAndAgeRangeRatioBySQL" --master local <find-gender-ratio-and-age-range-ratio-by-sql_2.11-1.0.jar路径> <input> <output>`
+
+* \<input>中仅含user_log_format1.csv和user_info_format1.csv
+* \<output>将含文件夹age range和gender，分别存储对性别及年龄段的人数、比例的统计
+
+### 运行结果
+
+#### 性别
+
+购买了商品的男性人数：121670，比例：29.87%
+购买了商品的女性人数：285638，比例：70.13%
+
+#### 年龄段
+
+年龄区间为<18的人数：24，比例：0.01%
+年龄区间为[18, 24]的人数：52420，比例：16.03%
+年龄区间为[25, 29]的人数：110952，比例：33.92%
+年龄区间为[30, 34]的人数：79649，比例：24.35%
+年龄区间为[35, 39]的人数：40601，比例：12.41%
+年龄区间为[40, 49]的人数：35257，比例：10.78%
+年龄区间为≥50的人数：8167，比例：2.50%
+
+## 4. MLlib预测新消费者在6个月内再次购买的概率
+
+#### 输入
+
+* 输入：data_format1文件夹
+
+#### 设计思路
+
+##### 数据读取
+
+
+利用`sparkSession.read`进行数据的分别读取（指定Schema），存储为DataFrame
+
+##### 特征工程
+
+特征的选取参考baseline
+
+参数说明：
+
+* infoDf：user_info_format1.csv读入的DataFrame
+* logDf：user_log_format1.csv读入的DataFrame
+* trainDf：train_format1.csv读入的DataFrame
+
+###### 用户年龄范围、性别
+
+1. 将trainDf和infoDf进行左内连接，获得infoLogDf
+
+###### 用户在该商家日志的总条数
+
+1. 对logDf关于user_id和seller_id进行groupBy与count操作，得到特征`total_tags`
+2. 将特征以join的方式加入为infoLogDf的列
+
+###### 用户浏览的该商家商品的数目、用户浏览的该商家商品种类的数目、用户浏览的天数
+
+以用户浏览的该商家商品的数目为例：
+
+1. 对logDf关于user_id、seller_id和item_id进行groupBy与count操作，得到browseDf
+2. 对browseDf关于user_id和seller_id进行groupBy与count操作，得到特征`browse_item_id`
+3. 将特征以join的方式加入为infoLogDf的列
+
+###### 用户对该商家单击、添加购物车、购买、添加收藏夹的次数
+
+1. 对logDf关于user_id、seller_id和action_type进行groupBy与count操作，得到browseDf
+2. 遍历action 0、1、2、3，对于每个action进行如下操作：新增flag列，定义udf聚合函数，若action_type == action，则flag为1，否则为0。新增click/shopping_cart/buy/favourite列，定义udf聚合函数，若flag == 1，则click/shopping_cart/buy/favourite为count值，否则为0。
+3. 将特征以join的方式加入为infoLogDf的列
+
+##### 模型的训练与预测
+
+* 使用MLlib中的LogisticRegressionWithSGD进行分类
+
+* 在预测前执行`lrModel.clearThreshold()`可以获得分类概率，而非获得类别标签
+
+##### 模型评价
+
+* 准确率：
+
+## 实验中遇到的问题
+
+1. 在bdkit中遇到进入其他同学控制台的情况，并且发现错乱时按Ctrl+L可以在我自己和其他同学的控制台间进行切换
+
+2. 在MLlib的任务中执行`sbt package`一直卡在update
+
+   ![](https://finclaw.oss-cn-shenzhen.aliyuncs.com/img/error.png)
+
+   换过源依然几千秒都没update完，后来发现居然是因为宿舍校园网太慢了，换个网络就成功了
 
 
 
--- RepurchasePrediction/src/main/scala/RepurchasePrediction.scala
+++ RepurchasePrediction/src/main/scala/RepurchasePrediction.scala
@@ -1,48 +1,142 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession, SQLContext}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.classification.LogisticRegressionWithSGD
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.types.StructType
 
 
 object RepurchasePrediction {
-  def getGenderUsers(line: String, gender: String): String = { //获得男users
-    val words = line.split(",")
-    if(gender == "male" && words(2) == "1"){
-      return words(0)
-    }
-    else if(gender == "female" && words(2) == "0"){
-      return words(0)
-    }
-    else{
-      return ""
-    }
+  def getCountFeature(target: String, logDf: DataFrame, infoLogDf: DataFrame): DataFrame = {
+    val browseDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf(target)).count()
+    var browseCountDf = browseDf.groupBy(browseDf("user_id"), browseDf("seller_id")).count()
+    browseCountDf = browseCountDf.withColumnRenamed("count", "browse_"+target).withColumnRenamed("seller_id", "merchant_id")
+    val newInfoLogDf = infoLogDf.join(browseCountDf, infoLogDf("user_id") === browseCountDf("user_id") && infoLogDf("merchant_id") === browseCountDf("merchant_id"), "left").drop(browseCountDf("user_id")).drop(browseCountDf("merchant_id"))
+    return newInfoLogDf
   }
-  
-  def getAgeUsers(line: String, ageRange: String): String = { //获得对应ageRange的users
-    val words = line.split(",")
-    if(ageRange == words(1)){
-      return words(0)
-    }
-    else{
-      return ""
-    }
-  }
-
   def main(args: Array[String]) {
     if (args.length < 2) {
       System.err.println("Usage: <input path> <output path>")
       System.exit(1)
     }
 
-    val conf = new SparkConf().setAppName("FindGenderRatioAndAgeRangeRatio")
-    val sc = new SparkContext(conf)
-    val train = sc.textFile(args(0))
+//    val conf = new SparkConf().setAppName("FindGenderRatioAndAgeRangeRatio")
+//    val sc = new SparkContext(conf)
+//    val sqlContext = new SQLContext(sc)
+//    val rawData = sc.textFile("../data_format1/train_format1.csv")
+//    val header = rawData.first() //csv的标题行
 
-    //val trainDf = train.map(_.split(",")).map { r => val trimmed = r.map(_.replaceAll("\"", "")) val label = trimmed(r.size - 1).toInt val features = trimmed.slice(4, r.size - 1).map(d => if (d == "?") 0.0 else d.toDouble) LabeledPoint(label, Vectors.dense(features)) } data.cache
+    val sparkSession: SparkSession = SparkSession.builder().master("local[*]").appName("RepurchasePrediction").getOrCreate()
 
-    //val numData = trainDf.count
-    
-    sc.stop()
+    val infoSchema = StructType(
+      List(
+        StructField("user_id", StringType),
+        StructField("age_range", DoubleType),
+        StructField("gender", DoubleType))
+    )
+
+    val logSchema = StructType(
+      List(
+        StructField("user_id", StringType),
+        StructField("item_id", StringType),
+        StructField("cat_id", StringType),
+        StructField("seller_id", StringType),
+        StructField("brand_id", StringType),
+        StructField("time_stamp", StringType),
+        StructField("action_type", StringType))
+    )
+
+    val trainSchema = StructType(
+      List(
+        StructField("user_id", StringType),
+        StructField("merchant_id", StringType),
+        StructField("label", DoubleType))
+    )
+
+    val infoPath = args(0) + "/user_info_format1.csv"
+    val logPath = args(0) +"/user_log_format1.csv"
+    val trainPath = args(0) +"/train_format1.csv"
+    val infoDf: DataFrame = sparkSession.read.format("csv").option("applySchema", "true").option("header", "true").option("sep", ",").schema(infoSchema).load(infoPath)
+    val logDf: DataFrame = sparkSession.read.format("csv").option("applySchema", "true").option("header", "true").option("sep", ",").schema(logSchema).load(logPath)
+    val trainDf: DataFrame = sparkSession.read.format("csv").option("applySchema", "true").option("header", "true").option("sep", ",").schema(trainSchema).load(trainPath)
+//    trainDf.printSchema()
+
+    //特征：用户年龄范围、性别
+    var infoLogDf = trainDf.join(infoDf, trainDf("user_id") === infoDf("user_id"), "left").drop(infoDf("user_id"))
+
+    //特征：用户在该商家日志的总条数
+    var totalLogCountDf = logDf.groupBy(logDf("user_id"),logDf("seller_id")).count()
+    totalLogCountDf = totalLogCountDf.withColumnRenamed("count", "total_logs").withColumnRenamed("seller_id", "merchant_id")
+    infoLogDf = infoLogDf.join(totalLogCountDf, infoLogDf("user_id") === totalLogCountDf("user_id") && infoLogDf("merchant_id") === totalLogCountDf("merchant_id"), "left").drop(totalLogCountDf("user_id")).drop(totalLogCountDf("merchant_id"))
+
+    infoLogDf = getCountFeature("item_id", logDf, infoLogDf)
+    infoLogDf = getCountFeature("cat_id", logDf, infoLogDf)
+    infoLogDf = getCountFeature("time_stamp", logDf, infoLogDf)
+//    //特征：用户浏览的该商家商品的数目
+//    val browseItemDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("item_id")).count()
+//    var browseItemCountDf = browseItemDf.groupBy(browseItemDf("user_id"), browseItemDf("seller_id")).count()
+//    browseItemCountDf = browseItemCountDf.withColumnRenamed("count", "browse_items").withColumnRenamed("seller_id", "merchant_id")
+//    infoLogDf = infoLogDf.join(browseItemCountDf, infoLogDf("user_id") === browseItemCountDf("user_id") && infoLogDf("merchant_id") === browseItemCountDf("merchant_id"), "left").drop(browseItemCountDf("user_id")).drop(browseItemCountDf("merchant_id"))
+//
+//    //特征：用户浏览的该商家商品的种类的数目
+//    val browseCategoryDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("cat_id")).count()
+//    var browseCategoryCountDf = browseCategoryDf.groupBy(browseCategoryDf("user_id"), browseCategoryDf("seller_id")).count()
+//    browseCategoryCountDf = browseCategoryCountDf.withColumnRenamed("count", "browse_categories").withColumnRenamed("seller_id", "merchant_id")
+//    infoLogDf = infoLogDf.join(browseCategoryCountDf, infoLogDf("user_id") === browseCategoryCountDf("user_id") && infoLogDf("merchant_id") === browseCategoryCountDf("merchant_id"), "left").drop(browseCategoryCountDf("user_id")).drop(browseCategoryCountDf("merchant_id"))
+//
+//    //特征：用户浏览的天数
+//    val browseDayDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("time_stamp")).count()
+//    var browseDayCountDf = browseDayDf.groupBy(browseDayDf("user_id"), browseDayDf("seller_id")).count()
+//    browseDayCountDf = browseDayCountDf.withColumnRenamed("count", "browse_days").withColumnRenamed("seller_id", "merchant_id")
+//    infoLogDf = infoLogDf.join(browseDayCountDf, infoLogDf("user_id") === browseDayCountDf("user_id") && infoLogDf("merchant_id") === browseDayCountDf("merchant_id"), "left").drop(browseDayCountDf("user_id")).drop(browseDayCountDf("merchant_id"))
+
+
+    infoLogDf.show()
+    val rawData = infoLogDf.rdd
+//    rawData.foreach(println)
+
+//    val assembler: VectorAssembler = new VectorAssembler().setInputCols(Array("sepal_length","sepal_width","petal_length","petal_width")).setOutputCol("features")
+//    val assmblerDf: DataFrame = assembler.transform(df)
+//    assmblerDf.show(false)
+//    //4-2将类别型class转变为数值型
+//    val stringIndex: StringIndexer = new StringIndexer().setInputCol("class").setOutputCol("label")
+//    val stingIndexModel: StringIndexerModel = stringIndex.fit(assmblerDf)
+//    val indexDf: DataFrame = stingIndexModel.transform(assmblerDf)
+
+
+
+//    val data = rawData.filter(line => line != header).map(_.split(",")).map{line =>
+//      val label = line(line.size - 1).toDouble
+//      val features = line.slice(0, line.size - 1).map(_.toDouble)
+//      LabeledPoint(label, Vectors.dense(features))}.cache
+//
+//    //分隔训练集和测试集
+//    val Array(trainData,testData): Array[RDD[LabeledPoint]] = data.randomSplit(Array(0.8,0.2))
+//    //val numData = trainDf.count
+//
+//    //训练模型
+//    val numIterations = 2 //迭代次数
+//    val lrModel = LogisticRegressionWithSGD.train(trainData, numIterations)
+//
+//    //测试
+//    val result: RDD[(Double, Double)] = testData.map(
+//      x=> {
+//        val prediction: Double = lrModel.predict(x.features)
+//        (x.label,prediction)
+//      }
+//    )
+//
+//    //模型评价
+//    val accuracy: Double = result.filter(x=>x._1==x._2).count().toDouble /result.count()
+//    println(accuracy)
+//    println("error", (1-accuracy))
+
+//    sc.stop()
   }
 }
