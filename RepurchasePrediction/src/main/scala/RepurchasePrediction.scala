eef16cd5fa95cf8bc72fba69493545a99460cead
jkq <1154609706@qq.com>
2020-12-27 23:26:59 +0800

    acc unfinish

--- RepurchasePrediction/src/main/scala/RepurchasePrediction.scala
+++ RepurchasePrediction/src/main/scala/RepurchasePrediction.scala
@@ -2,10 +2,11 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession, SQLContext}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.classification.LogisticRegressionWithSGD
+import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithSGD, NaiveBayes, SVMWithSGD}
+import org.apache.spark.sql.functions.udf
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.DoubleType
@@ -16,21 +17,18 @@ object RepurchasePrediction {
   def getCountFeature(target: String, logDf: DataFrame, infoLogDf: DataFrame): DataFrame = {
     val browseDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf(target)).count()
     var browseCountDf = browseDf.groupBy(browseDf("user_id"), browseDf("seller_id")).count()
-    browseCountDf = browseCountDf.withColumnRenamed("count", "browse_"+target).withColumnRenamed("seller_id", "merchant_id")
-    val newInfoLogDf = infoLogDf.join(browseCountDf, infoLogDf("user_id") === browseCountDf("user_id") && infoLogDf("merchant_id") === browseCountDf("merchant_id"), "left").drop(browseCountDf("user_id")).drop(browseCountDf("merchant_id"))
+    browseCountDf = browseCountDf.withColumnRenamed("count", "browse_"+target)
+    val newInfoLogDf = infoLogDf.join(browseCountDf, infoLogDf("user_id") === browseCountDf("user_id") && infoLogDf("merchant_id") === browseCountDf("seller_id"), "left").drop(browseCountDf("user_id")).drop(browseCountDf("seller_id"))
     return newInfoLogDf
   }
   def main(args: Array[String]) {
-    if (args.length < 2) {
-      System.err.println("Usage: <input path> <output path>")
-      System.exit(1)
-    }
+   if (args.length < 1) {
+     System.err.println("Usage: <input path>")
+     System.exit(1)
+   }
 
-//    val conf = new SparkConf().setAppName("FindGenderRatioAndAgeRangeRatio")
-//    val sc = new SparkContext(conf)
-//    val sqlContext = new SQLContext(sc)
-//    val rawData = sc.textFile("../data_format1/train_format1.csv")
-//    val header = rawData.first() //csv的标题行
+    val conf = new SparkConf().setAppName("RepurchasePrediction")
+    val sc = new SparkContext(conf)
 
     val sparkSession: SparkSession = SparkSession.builder().master("local[*]").appName("RepurchasePrediction").getOrCreate()
 
@@ -59,14 +57,15 @@ object RepurchasePrediction {
         StructField("label", DoubleType))
     )
 
+    //数据读取
     val infoPath = args(0) + "/user_info_format1.csv"
     val logPath = args(0) +"/user_log_format1.csv"
     val trainPath = args(0) +"/train_format1.csv"
     val infoDf: DataFrame = sparkSession.read.format("csv").option("applySchema", "true").option("header", "true").option("sep", ",").schema(infoSchema).load(infoPath)
     val logDf: DataFrame = sparkSession.read.format("csv").option("applySchema", "true").option("header", "true").option("sep", ",").schema(logSchema).load(logPath)
     val trainDf: DataFrame = sparkSession.read.format("csv").option("applySchema", "true").option("header", "true").option("sep", ",").schema(trainSchema).load(trainPath)
-//    trainDf.printSchema()
 
+    //特征工程
     //特征：用户年龄范围、性别
     var infoLogDf = trainDf.join(infoDf, trainDf("user_id") === infoDf("user_id"), "left").drop(infoDf("user_id"))
 
@@ -75,68 +74,93 @@ object RepurchasePrediction {
     totalLogCountDf = totalLogCountDf.withColumnRenamed("count", "total_logs").withColumnRenamed("seller_id", "merchant_id")
     infoLogDf = infoLogDf.join(totalLogCountDf, infoLogDf("user_id") === totalLogCountDf("user_id") && infoLogDf("merchant_id") === totalLogCountDf("merchant_id"), "left").drop(totalLogCountDf("user_id")).drop(totalLogCountDf("merchant_id"))
 
-    infoLogDf = getCountFeature("item_id", logDf, infoLogDf)
-    infoLogDf = getCountFeature("cat_id", logDf, infoLogDf)
-    infoLogDf = getCountFeature("time_stamp", logDf, infoLogDf)
 //    //特征：用户浏览的该商家商品的数目
-//    val browseItemDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("item_id")).count()
-//    var browseItemCountDf = browseItemDf.groupBy(browseItemDf("user_id"), browseItemDf("seller_id")).count()
-//    browseItemCountDf = browseItemCountDf.withColumnRenamed("count", "browse_items").withColumnRenamed("seller_id", "merchant_id")
-//    infoLogDf = infoLogDf.join(browseItemCountDf, infoLogDf("user_id") === browseItemCountDf("user_id") && infoLogDf("merchant_id") === browseItemCountDf("merchant_id"), "left").drop(browseItemCountDf("user_id")).drop(browseItemCountDf("merchant_id"))
+//    infoLogDf = getCountFeature("item_id", logDf, infoLogDf)
 //
 //    //特征：用户浏览的该商家商品的种类的数目
-//    val browseCategoryDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("cat_id")).count()
-//    var browseCategoryCountDf = browseCategoryDf.groupBy(browseCategoryDf("user_id"), browseCategoryDf("seller_id")).count()
-//    browseCategoryCountDf = browseCategoryCountDf.withColumnRenamed("count", "browse_categories").withColumnRenamed("seller_id", "merchant_id")
-//    infoLogDf = infoLogDf.join(browseCategoryCountDf, infoLogDf("user_id") === browseCategoryCountDf("user_id") && infoLogDf("merchant_id") === browseCategoryCountDf("merchant_id"), "left").drop(browseCategoryCountDf("user_id")).drop(browseCategoryCountDf("merchant_id"))
+//    infoLogDf = getCountFeature("cat_id", logDf, infoLogDf)
 //
 //    //特征：用户浏览的天数
-//    val browseDayDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("time_stamp")).count()
-//    var browseDayCountDf = browseDayDf.groupBy(browseDayDf("user_id"), browseDayDf("seller_id")).count()
-//    browseDayCountDf = browseDayCountDf.withColumnRenamed("count", "browse_days").withColumnRenamed("seller_id", "merchant_id")
-//    infoLogDf = infoLogDf.join(browseDayCountDf, infoLogDf("user_id") === browseDayCountDf("user_id") && infoLogDf("merchant_id") === browseDayCountDf("merchant_id"), "left").drop(browseDayCountDf("user_id")).drop(browseDayCountDf("merchant_id"))
+//    infoLogDf = getCountFeature("time_stamp", logDf, infoLogDf)
 
+    //特征：用户浏览的该商家商品的数目
+    val browseItemDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("item_id")).count()
+    var browseItemCountDf = browseItemDf.groupBy(browseItemDf("user_id"), browseItemDf("seller_id")).count()
+    browseItemCountDf = browseItemCountDf.withColumnRenamed("count", "browse_items").withColumnRenamed("seller_id", "merchant_id")
+    infoLogDf = infoLogDf.join(browseItemCountDf, infoLogDf("user_id") === browseItemCountDf("user_id") && infoLogDf("merchant_id") === browseItemCountDf("merchant_id"), "left").drop(browseItemCountDf("user_id")).drop(browseItemCountDf("merchant_id"))
 
-    infoLogDf.show()
-    val rawData = infoLogDf.rdd
-//    rawData.foreach(println)
+    //特征：用户浏览的该商家商品的种类的数目
+    val browseCategoryDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("cat_id")).count()
+    var browseCategoryCountDf = browseCategoryDf.groupBy(browseCategoryDf("user_id"), browseCategoryDf("seller_id")).count()
+    browseCategoryCountDf = browseCategoryCountDf.withColumnRenamed("count", "browse_categories").withColumnRenamed("seller_id", "merchant_id")
+    infoLogDf = infoLogDf.join(browseCategoryCountDf, infoLogDf("user_id") === browseCategoryCountDf("user_id") && infoLogDf("merchant_id") === browseCategoryCountDf("merchant_id"), "left").drop(browseCategoryCountDf("user_id")).drop(browseCategoryCountDf("merchant_id"))
 
-//    val assembler: VectorAssembler = new VectorAssembler().setInputCols(Array("sepal_length","sepal_width","petal_length","petal_width")).setOutputCol("features")
-//    val assmblerDf: DataFrame = assembler.transform(df)
-//    assmblerDf.show(false)
-//    //4-2将类别型class转变为数值型
-//    val stringIndex: StringIndexer = new StringIndexer().setInputCol("class").setOutputCol("label")
-//    val stingIndexModel: StringIndexerModel = stringIndex.fit(assmblerDf)
-//    val indexDf: DataFrame = stingIndexModel.transform(assmblerDf)
+    //特征：用户浏览的天数
+    val browseDayDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("time_stamp")).count()
+    var browseDayCountDf = browseDayDf.groupBy(browseDayDf("user_id"), browseDayDf("seller_id")).count()
+    browseDayCountDf = browseDayCountDf.withColumnRenamed("count", "browse_days").withColumnRenamed("seller_id", "merchant_id")
+    infoLogDf = infoLogDf.join(browseDayCountDf, infoLogDf("user_id") === browseDayCountDf("user_id") && infoLogDf("merchant_id") === browseDayCountDf("merchant_id"), "left").drop(browseDayCountDf("user_id")).drop(browseDayCountDf("merchant_id"))
 
+    //特征：用户对该商家单击、添加购物车、购买、添加收藏夹的次数
+    var actionDf = logDf.groupBy(logDf("user_id"),logDf("seller_id"),logDf("action_type")).count()
 
+    for(i <- 0 to 3){
+      val action = i match  {
+        case 0 => "click"
+        case 1 => "shopping_cart"
+        case 2 => "buy"
+        case 3=> "favourite"
+      }
+      val addFlagCol = udf((arg: String) => {if (arg == String.valueOf(i)) 1 else 0})
+      actionDf = actionDf.withColumn("flag", addFlagCol(actionDf("action_type")))
 
-//    val data = rawData.filter(line => line != header).map(_.split(",")).map{line =>
-//      val label = line(line.size - 1).toDouble
-//      val features = line.slice(0, line.size - 1).map(_.toDouble)
-//      LabeledPoint(label, Vectors.dense(features))}.cache
+      val addActionCountCol = udf((flag: Int, count: Long) => {if (flag == 1) count else 0.toLong})
+      actionDf = actionDf.withColumn(action, addActionCountCol(actionDf("flag"), actionDf("count")))
+      actionDf = actionDf.drop("flag")
+    }
+    actionDf = actionDf.drop("count").drop("action_type")
+    infoLogDf = infoLogDf.join(actionDf, infoLogDf("user_id") === actionDf("user_id") && infoLogDf("merchant_id") === actionDf("seller_id"), "left").drop(actionDf("user_id")).drop(actionDf("seller_id"))
+
+//    infoLogDf.repartition(1).write.format("com.databricks.spark.csv")
+//      .option("header", "true")//在csv第一行有属性"true"，没有就是"false"
+//      .option("delimiter",",")//默认以","分割
+//      .save("csv")
+//    infoLogDf.show()
+//    infoLogDf.printSchema()
 //
-//    //分隔训练集和测试集
-//    val Array(trainData,testData): Array[RDD[LabeledPoint]] = data.randomSplit(Array(0.8,0.2))
-//    //val numData = trainDf.count
-//
-//    //训练模型
-//    val numIterations = 2 //迭代次数
-//    val lrModel = LogisticRegressionWithSGD.train(trainData, numIterations)
-//
-//    //测试
-//    val result: RDD[(Double, Double)] = testData.map(
-//      x=> {
-//        val prediction: Double = lrModel.predict(x.features)
-//        (x.label,prediction)
-//      }
-//    )
-//
-//    //模型评价
-//    val accuracy: Double = result.filter(x=>x._1==x._2).count().toDouble /result.count()
-//    println(accuracy)
-//    println("error", (1-accuracy))
+//    infoLogDf.coalesce(1).write.format("com.databricks.spark.csv").save("test")
 
-//    sc.stop()
+    val rawData = infoLogDf.rdd.map(line => line.toString).map(line => line.substring(1, line.size - 1))
+    //rawData.saveAsTextFile("text")
+
+    val data = rawData.map(_.split(",")).map{line =>
+      val label = line(2).toDouble
+      val features = line.slice(3, line.size).map(_.toDouble)
+      LabeledPoint(label, Vectors.dense(features))}.cache
+
+    //分割训练集和测试集
+    val Array(trainData, testData): Array[RDD[LabeledPoint]] = data.randomSplit(Array(0.8,0.2))
+
+    //训练模型.clearThreshold()
+    val numIterations = 3 //迭代次数
+    val lrModel = LogisticRegressionWithSGD.train(trainData, numIterations)
+    //lrModel.clearThreshold()//
+    lrModel.save(sc, "model_new")
+
+
+    //测试
+    val result: RDD[(Double, Double)] = testData.map(
+      x=> {
+        val prediction: Double = lrModel.predict(x.features)
+        (x.label,prediction)
+      }
+    )
+
+    //模型评价
+    val accuracy: Double = result.filter(x => x._1 == x._2).count().toDouble / result.count()
+    println("Accuracy: ", accuracy)
+    println("Error: ", (1-accuracy))
+
+    sparkSession.stop()
   }
 }
